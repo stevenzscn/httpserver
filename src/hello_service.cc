6da769021103be8436b5369b2e69d59c214ee84c
Steven Zhang <stevenzscn@gmail.com>
2019-03-04 19:28:17 +0800

    hello_server

--- src/hello_service.cc
+++ src/hello_service.cc
@@ -6,12 +6,12 @@
 
 int HelloService::handle(std::shared_ptr<Context> context) {
     
-    boost::property_tree::ptree hello;
-    hello.put("uri", context->uri_);
-    hello.put("content", context->req_body_);
+    boost::property_tree::ptree hello_res;
+    hello_res.put("uri", context->uri_);
+    hello_res.put("content", context->req_body_);
 
     std::stringstream ss;
-    boost::property_tree::json_parser::write_json(ss, hello);
+    boost::property_tree::json_parser::write_json(ss, hello_res);
 
     context->content_type_ = "application/json";
     context->res_body_ = ss.str();
